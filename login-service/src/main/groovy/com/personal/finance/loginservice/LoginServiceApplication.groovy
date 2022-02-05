a326d10028140e0862cc629cb21f5e5118fd59dc
Dhiraj Sable <sable.dhiraj@gmail.com>
2022-02-04 17:16:59 -0800

    Project Skeleton

---
+++ login-service/src/main/groovy/com/personal/finance/loginservice/LoginServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.personal.finance.loginservice
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LoginServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(LoginServiceApplication, args)
+    }
+
+}
